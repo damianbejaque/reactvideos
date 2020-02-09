da89d939b4916eccfbdc3814c21d3edc0c296b14
Damian Bejarano <damianbejaque@gmail.com>
2020-02-08 23:53:43 -0500

    Creacion server local por medio de json-server y agregar distintos hooks a la aplicacion

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
-
+import useInitialState from "../hooks/useInitialState"
 import "../assets/styles/App.scss";
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
-        <Categories title="Ejemplo">
-            <Carousel >
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const API = "http://localhost:3000/initalState";
 
-        <Categories title="Ejemplo 2">
-            <Carousel >
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-)
+const App = () => {
+    const inititalState = useInitialState(API);
+
+
+    return inititalState.length === 0 ? <h1>Loading...</h1> : (
+        <div className="App">
+            <Header />
+            <Search />
+            {inititalState.mylist.length > 0 &&
+                <Categories title="Mi Lista">
+                    <Carousel >
+                    </Carousel>
+                </Categories>
+            }
+            <Categories title="Tendencias">
+                <Carousel >
+                    {inititalState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales">
+                <Carousel >
+                    {inititalState.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    )
+};
 export default App;
